92086379175b9eda11c6c506f96d472203bdf7f5
Kevin <kevinsadi@live.com>
2021-07-11 00:14:15 -0400

    added rabbit transition, time to finish the flow

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -4,12 +4,19 @@ using System;
 
 public class AudioManager : MonoBehaviour {
     public Sound[] sounds;
+    
+    public GameObject rabbitAndGrass;
+    private Boolean rabbitNotSpawned = true;
+
 
     /// <summary>
     /// Awake is called when the script instance is being loaded.
     /// </summary>
     void Awake()
     {
+        // make it so you can't see the rabbit and grass till first song ends
+        rabbitAndGrass.SetActive(false);
+
         foreach (Sound s in sounds) {
             // this makes the actual AudioSource component
             s.source = gameObject.AddComponent<AudioSource>();
@@ -18,6 +25,7 @@ public class AudioManager : MonoBehaviour {
             s.source.clip = s.clip;
             s.source.volume = s.volume;
             s.source.pitch = s.pitch;
+            s.source.name = s.name;
         }
     }
 
@@ -30,5 +38,22 @@ public class AudioManager : MonoBehaviour {
     {
         Sound selectedSound = Array.Find(sounds, sound => sound.name == name);
         selectedSound.source.Play();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        // when the first song ends, I want to spawn the rabbit. This is not the place to do this, but this is a game jam.
+        Sound introSound = Array.Find(sounds, sound => sound.name == "Intro");
+
+        if (rabbitNotSpawned) {
+            if (!introSound.source.isPlaying) {
+                print("Audio Source Ended");
+                rabbitAndGrass.SetActive(true);
+                rabbitNotSpawned = false;
+            }
+        }
     }
 }
